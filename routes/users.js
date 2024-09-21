db18fa299ce5fe3cd3a2fd76d941c3e79e2dece3
Aivaxela <rmarcum13@gmail.com>
2024-09-21 13:10:01 -0400

    add cert module and update secret

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
 const { getCurrentUser, updateProfile } = require("../controllers/users");
 const { auth } = require("../middleware/auth");
-const {
-  validateId,
-  validateUserInfoOnUpdate,
-} = require("../middleware/validation");
+const { validateUserInfoOnUpdate } = require("../middleware/validation");
 
-router.get("/me", auth, validateId, getCurrentUser);
+router.get("/me", auth, getCurrentUser);
 router.patch("/me", auth, validateUserInfoOnUpdate, updateProfile);
 
 module.exports = router;
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  JWT_SECRET: "secret",
-};
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
+module.exports = { JWT_SECRET };
