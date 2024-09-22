a239e8043eb07aaf695eb586af1d2aa8f1b7154e
Riley Marcum <rmarcum13@gmail.com>
2024-09-22 17:01:57 -0400

    temp remove limiter

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 // app.use(helmet());
-app.use(limiter);
+// app.use(limiter);
 
 app.use(requestLogger);
 
