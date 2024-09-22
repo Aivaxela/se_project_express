e03a4f84a43d234ab5bc7334f6357cc9579dae74
Riley Marcum <rmarcum13@gmail.com>
2024-09-22 13:03:20 -0400

    standarize Joi validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -23,20 +23,22 @@ const validateId = (value, helpers) => {
 };
 
 module.exports.validateNewClothingItem = celebrate({
-  body: Joi.object()
-    .keys({
-      name: Joi.string().required().min(2).max(30).messages({
-        "string.min": 'The minimum length of the "name" field is 2',
-        "string.max": 'The maximum length of the "name" field is 30',
-        "string.empty": 'The "name" field must be filled in',
-      }),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
 
-      imageUrl: Joi.string().required().custom(validateURL).messages({
-        "string.empty": 'The "imageUrl" field must be filled in',
-        "string.uri": 'the "imageUrl" field must be a valid url',
-      }),
-    })
-    .unknown(true),
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+
+    weather: Joi.string().required().valid("hot", "warm", "cold").messages({
+      "string.empty": 'The "weather" field must be filled in',
+    }),
+  }),
 });
 
 module.exports.validateNewUser = celebrate({
