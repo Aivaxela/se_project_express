115e6fe4fa0881bcf3f18bb1f92bb78f5314a8b3
Riley Marcum <rmarcum13@gmail.com>
2024-09-22 16:45:51 -0400

    implement helmet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
 const { errors } = require("celebrate");
 const { limiter } = require("./middleware/limiter");
 const { errorHandler, errorSender } = require("./middleware/error-handler");
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
 app.use(limiter);
 
 app.use(requestLogger);
